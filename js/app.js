68de5c78f937780940186a287c2f7a2986660905
Nouran Samy <only1nour@ymail.com>
2018-04-10 19:40:29 +0200

    Initial commit
    Still lacking responsiveness and hover effects

---
+++ js/app.js
@@ -0,0 +1,106 @@
+
+/* Declaration of Global Variables */
+
+var input = document.getElementById("input"),
+    digits = document.querySelectorAll("span"),
+    arr = [];
+
+/* For loop to add a click event to every button and check for its type */
+
+for(var i = 0; i < digits.length; i++){
+    digits[i].addEventListener('click', function(){
+        if(this.classList.contains('operator')){
+            input.textContent += " " + this.textContent + " ";
+        }
+        else if(this.classList.contains('number')){
+            input.textContent += this.textContent;
+        }
+        else if (this.id === 'negative'){
+            input.textContent += "-";
+        }
+        else if(this.id === 'equal'){
+            calculate(input.textContent);
+        }
+        else{
+            input.textContent = "";
+        }
+    });
+}
+
+/* 
+    Function called when clicking the equal button to calculate the equation 
+    by splitting it into an array and checking for each operator
+*/
+
+function calculate(str){
+    var result;
+
+    arr = str.split(" ");
+    console.log(arr);
+
+    /* 
+        For loop to iterate through the array and find every multiplication or 
+        division operator 
+    */
+
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] === '/'){
+            result = determineOperation(i, arr[i]);
+            i--;
+        }
+        else if (arr[i] === '*'){
+            result = determineOperation(i, arr[i]);
+            i--;
+        }
+        else{
+            continue;
+        }
+    }
+
+    /* 
+        For loop to iterate through the array and find every addition or 
+        subtraction operator 
+    */
+
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i] === '+'){
+            result = determineOperation(i, arr[i]);
+            i--;
+        }
+        else if (arr[i] === '-'){
+            result = determineOperation(i, arr[i]);
+            i--;
+        }
+        else{
+            continue;
+        }
+    }
+
+    input.textContent = result;
+}
+
+/* Function to calculate the end result according to operator */
+
+function determineOperation(index, operator){
+
+    switch (operator){
+        case '/':
+            result = Number(arr[index - 1]) / Number(arr[index + 1]);
+            console.log(result + "divide");
+            break;
+        case '*':
+            result = Number(arr[index - 1]) * Number(arr[index + 1]);
+            console.log(result + "times");
+            break;
+        case '+':
+            result = Number(arr[index - 1]) + Number(arr[index + 1]);
+            break;
+        case '-':
+            result = Number(arr[index - 1]) - Number(arr[index + 1]);
+            break;
+    }
+    /* Removing the two numbers and the operator after calculation and replacing them with the result */
+    arr.splice(index - 1, 3, result);
+
+    return result;
+}
